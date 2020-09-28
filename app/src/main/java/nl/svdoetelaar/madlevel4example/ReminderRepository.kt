7b21351c3fd70fe73df6d09a4fd0bd83c9387e64
Sander <sandervandenoetelaar@hotmail.com>
2020-09-28 17:32:26 +0200

    completed steps 1 and 2

---
+++ app/src/main/java/nl/svdoetelaar/madlevel4example/ReminderRepository.kt
@@ -0,0 +1,28 @@
+package nl.svdoetelaar.madlevel4example
+
+import android.content.Context
+
+public class ReminderRepository(context: Context) {
+    private var reminderDao: ReminderDao
+
+    init {
+        val reminderRoomDatabase = ReminderRoomDatabase.getDatabase(context)
+        reminderDao = reminderRoomDatabase!!.reminderDao()
+    }
+
+    fun getAllReminders(): List<Reminder> {
+        return reminderDao.getAllReminders()
+    }
+
+    fun insertReminder(reminder: Reminder) {
+        reminderDao.insertReminder(reminder)
+    }
+
+    fun updateReminder(reminder: Reminder) {
+        reminderDao.updateReminder(reminder)
+    }
+
+    fun deleteReminder(reminder: Reminder) {
+        reminderDao.deleteReminder(reminder)
+    }
+}
